1e0df23c1440dd0b43a78c9174083ffc56366d64
meghanab <b11meghana@gmail.com>
2025-03-05 14:26:36 +0530

    Added FindtheIndexofAFirstOccurrenceinAString.java

---
+++ TwoPointers/FindtheIndexofAFirstOccurrenceinAString.java
@@ -0,0 +1,24 @@
+//LeetCode Solution: 28. Find the Index of the First Occurrence in a String
+//Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=problem-list-v2&envId=two-pointers
+
+//Time Complexity:O(m*n)
+//Space Complexity:O(1)
+
+class Solution{
+public static int strStr(String haystack, String needle){
+  int m = haystack.length();
+  int n = needle.length();
+
+  for(int i=0;i<=m-n;i++){
+    int j=0;
+    while(j<n && haystack.charAt(i+j)==needle.charAt(j)){
+      j++;
+    }
+    if(j==n){
+      return i;
+    }
+  }
+  return -1;
+
+}
+}
