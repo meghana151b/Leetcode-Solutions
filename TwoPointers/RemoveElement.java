1c96e3ff442542ab6665ab6e6ad69f8b662c4b3d
meghanab <b11meghana@gmail.com>
2025-03-08 21:15:18 +0530

    Added RemoveElement.java

--- TwoPointers/RemoveElement.java
+++ TwoPointers/RemoveElement.java
@@ -1,5 +1,7 @@
-// LeetCode Program: Remove Element
-// LeetCode Link   : https://leetcode.com/problems/remove-element/submissions/1557242239/?envType=problem-list-v2&envId=two-pointers
+//LeetCode Program: Remove Element
+//LeetCode Link   : https://leetcode.com/problems/remove-element/submissions/1557242239/?envType=problem-list-v2&envId=two-pointers
+//Time Complexity :O(n)
+//Space Complexity:O(1)
 
 
 class Solution {
@@ -13,6 +15,24 @@ class Solution {
         }
         return i; // i represents new array length
         
+    }
+    public static void main(String[] main){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter total number of arrays");
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        System.out.println("Enter value to be removed");
+        int val=sc.nextInt();
+       int newLength= removeElement(nums,val);
+        System.out.println("New Length: "+newLength);
+        System.out.print("Modified array: ");
+        for(int i=0;i<newLength;i++){
+            System.out.print(nums[i]+" ");
+        }
     }
 }
   
