53eb96f2d531f72f25798370dcc468739788aa44
meghanab <b11meghana@gmail.com>
2025-03-21 23:51:01 +0530

    Added DuplicateZeros.java

---
+++ TwoPointers/DuplicateZeros.java
@@ -0,0 +1,36 @@
+//LeetCode Program:Duplicate Zeros
+//Link:https://leetcode.com/problems/duplicate-zeros/?envType=problem-list-v2&envId=two-pointers
+
+//Time Complexity:O(N)
+//Space Complexity:O(1)
+
+
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int countZeros = 0;
+
+        // Count the number of zeros
+        for (int num : arr) {
+            if (num == 0) countZeros++;
+        }
+
+        int i = n - 1, j = n + countZeros - 1;
+
+        // Traverse from the end and shift elements
+        while (i >= 0) {
+            if (j < n) {
+                arr[j] = arr[i]; // Copy element if within bounds
+            }
+            j--;
+
+            if (arr[i] == 0) { // If zero, duplicate it
+                if (j < n) {
+                    arr[j] = 0;
+                }
+                j--;
+            }
+            i--;
+        }
+    }
+}
