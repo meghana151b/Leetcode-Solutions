0bdae888a7cb4bf155978152ae3b979a8c767bd3
meghanab <b11meghana@gmail.com>
2025-03-23 23:22:25 +0530

    Added LengthOfLastWord.java

---
+++ Strings/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+//LeetCode Program:Length of last word
+//Link:https://leetcode.com/problems/length-of-last-word/submissions/1583490387/?envType=problem-list-v2&envId=string
+
+//Time Complexity:O(N)
+//Space Complexity:O(1)
+
+class Program{
+  public int lengthOfLastWord(String s){
+    int length=0;
+    int i=s.length()-1;
+    while(i>=0 && s.charAt(i)==' '){
+      i--;
+    }
+    while(i>=0 && s.charAt(i)!=' '){
+      length++;
+      i--;
+    }
+    return length;
+  }
+}
+
