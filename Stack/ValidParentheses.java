8fa6892a95794cdbc46c2aa5f4510283026a7a9e
meghanab <b11meghana@gmail.com>
2025-06-17 23:25:03 +0530

    Uploaded ValidParentheses.java

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+//Leetcode:Valid Parentheses
+//Link:https://leetcode.com/problems/valid-parentheses/
+
+//Time Complexity:O(n)
+//Space Complexity:O(n)
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
