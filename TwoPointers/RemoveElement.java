4e436c4ecff0594fbe5d38e0768138e90d61b2ec
meghanab <b11meghana@gmail.com>
2025-07-07 23:56:00 +0530

    Update RemoveElement.java

--- TwoPointers/RemoveElement.java
+++ TwoPointers/RemoveElement.java
@@ -6,16 +6,20 @@
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i=0; // This pointer tracks where valid elements should go
+        if(nums.length==0) return 0;
+
+        int i=0;
         for(int j=0;j<nums.length;j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j]; //Move valid elements to position i
+            if(nums[j] != val){
+                nums[i]=nums[j];
                 i++;
             }
         }
-        return i; // i represents new array length
+        return i;
+
         
     }
+
     public static void main(String[] main){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter total number of arrays");
